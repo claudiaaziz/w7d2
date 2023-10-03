484b283b64ea431a183ea05f57b53cb1be89fa15
claudiaaziz <claudiahannahh@gmail.com>
2023-10-03 17:28:52 -0400

    Add actions to SessionsController

--- practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
     def new
-        
+        @user = User.new
+        render :new
     end
 
     def create
-        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
         debugger
 
         if @user
@@ -14,5 +15,11 @@ class SessionsController < ApplicationController
             flash.now[:errors] = ['Login Failed']
         end
     end 
+
+    def destroy
+        logout!
+        flash[:messages] = ["Logout Successful"]
+        redirect_to new_session_url
+    end
 end
  
